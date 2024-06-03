1550ce0fcdbd39e3099396879259044830aae3eb
nasubi <r916nis@gmail.com>
2024-06-04 02:07:47 +0900

    15

--- src/scripts/station15.js
+++ src/scripts/station15.js
@@ -3,14 +3,34 @@ async function getData() {
   // ３秒後にフルネームのリストを表示されるようにしましょう。
   // 最初から表示されていると、クリアになりません。
   const userList = [
-    { id: 1, first_name: '優', family_name: '大木', affiliation: 'TechTrain', is_student: false },
-    { id: 2, first_name: '太郎', family_name: '山田', affiliation: 'HogeHoge大学', is_student: true }
+    {
+      id: 1,
+      first_name: "優",
+      family_name: "大木",
+      affiliation: "TechTrain",
+      is_student: false,
+    },
+    {
+      id: 2,
+      first_name: "太郎",
+      family_name: "山田",
+      affiliation: "HogeHoge大学",
+      is_student: true,
+    },
   ];
-  const result = await test()
-  return await result
+  const result = await test(userList)
+  return result;
 }
 
-function test() {
-  return
+async function test(userList) {
+  let userFullName = [];
+  return await new Promise((resolve) => {
+    setTimeout(() => {
+      userFullName = userList.map((user) => {
+        user.full_name = user.family_name + " " + user.first_name;
+        return user;
+      });
+      resolve(userFullName);
+    }, 3000);
+  });
 }
-
